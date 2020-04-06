64141edaa604981c9b12ad9c734246d05be37b2a
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-06 11:11:44 -0700

    rewrote js and css to fix flashcards, now working

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
-    $(".flashcard-front").click(function() {
-        $("flashcard-front").removeClass();
-        $("flashcard-front").addClass("flashcard-back");
-    });
-});
-
-$(document).ready(function() {
-    $(".flashcard-back").click(function() {
-        $("flashcard-front").removeClass();
-        $("flashcard-back").addClass("flashcard-front");
+    $("#flashcard-front").click(function() {
+        console.log("test")
+        $("#flashcard-back").toggle();
     });
 });
