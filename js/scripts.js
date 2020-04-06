54b6b5138aee1a6498c78fe7f2b3e4a00fa6e79a
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-06 16:17:57 -0700

    created more flashcards in html

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,38 @@ $(document).ready(function() {
         $("#flashcard-back").toggle();
     });
 });
+
+$(document).ready(function() {
+    $("#flashcard-front2").click(function() {
+        console.log("test")
+        $("#flashcard-back2").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#flashcard-front3").click(function() {
+        console.log("test")
+        $("#flashcard-back3").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#flashcard-front4").click(function() {
+        console.log("test")
+        $("#flashcard-back4").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#flashcard-front5").click(function() {
+        console.log("test")
+        $("#flashcard-back5").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#flashcard-front6").click(function() {
+        console.log("test")
+        $("#flashcard-back6").toggle();
+    });
+});
